2069191ef9ef0d0c822c77ee0b9f8f43ac7a9aef
Joe-Brady <joebradydev@gmail.com>
2025-01-01 17:07:57 +0000

    Replace gradient CSS with avif

--- src/components/MulticolourSplash/MulticolourSplash.tsx
+++ src/components/MulticolourSplash/MulticolourSplash.tsx
@@ -1,10 +1,18 @@
 import { ReactNode } from "react";
 import styles from "./component.module.css";
+import Image from "next/image";
 
 type Props = {
   children: ReactNode;
 };
 
 export const MulticolourSplash = ({ children }: Props) => {
-  return <div className={styles.gradient}>{children}</div>;
+  return (
+    <div className={styles.splashContainer}>
+      <div className={styles.gradient}>
+        <Image src="/splash.avif" alt="Colour splash" fill />;
+      </div>
+      {children}
+    </div>
+  );
 };
